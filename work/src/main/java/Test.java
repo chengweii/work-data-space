6965970389d5566b843497ab9853962a2444ca3f
chengwei2 <chengwei2@JXWBJ1207261100.jiuxian.local>
2017-10-18 15:32:46 +0800

    update

--- work/src/main/java/Test.java
+++ work/src/main/java/Test.java
@@ -1,11 +1,30 @@
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Test {
+	public static Timer timer = new Timer();
+
 	public static void main(String[] args) {
 		Calendar c = Calendar.getInstance();
-        c.setTime(new Date());
-        c.add(1, 10);
-        System.out.println(StatementStatusType.valueOf("FINANCE_REJECT")==StatementStatusType.FINANCE_REJECT);  
+		c.setTime(new Date());
+		c.add(1, 10);
+		System.out.println("");
+		testTask();
+	}
+
+	public static void testTask() {
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				try {
+					long sleeptime = (long) (Math.random() * 5000);
+					Thread.sleep(sleeptime);
+					System.out.println(sleeptime);
+				} catch (Exception e) {
+				}
+			}
+		}, 5000, 1000);
 	}
 }
