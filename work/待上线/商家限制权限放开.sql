805621b557092fa803a87f3a89d5a4eb620b9ae9
chengwei2 <chengwei2@jiuxian.com>
2018-04-28 11:51:51 +0800

    update

--- work/待上线/商家限制权限放开.sql
+++ work/待上线/商家限制权限放开.sql
@@ -17,3 +17,8 @@ update zntg_user.tg_UserQualifications t set t.MerchantFlag = 1 where t.ID  = '3
 update zntg_jxweb.tg_Merchant t set t.status = 2 where t.ID = '1041';
 # 修改用户资质状态由 准备取消合作的商家 到 正常商家
 update zntg_user.tg_UserQualifications t set t.MerchantFlag = 1 where t.ID  = '8576';​​
+
+# 修改店铺状态由 限制权限 到 审核通过
+update zntg_jxweb.tg_Merchant t set t.status = 2 where t.ID IN ('915','838','439');
+# 修改用户资质状态由 准备取消合作的商家 到 正常商家
+update zntg_user.tg_UserQualifications t set t.MerchantFlag = 1 where t.ID  IN ('3524','3306','1528');
