d10d0ec224bd30ac7b12b165d7268ee74192c964
chengweii <295999757@qq.com>
2018-08-06 17:11:11 +0800

    init

--- work/待上线/订单商品优惠券余额分摊/订单商品优惠券余额分摊.sql
+++
@@ -1,7 +0,0 @@
-USE zntg_order;
-
-#1.新增订单商品优惠券分摊金额、余额分摊金额字段
-ALTER TABLE `tg_HaveProduct`
-ADD COLUMN `use_coupon_price`  decimal(10,2) NULL COMMENT '优惠券分摊金额' AFTER `shop_service_charge_rate`,
-ADD COLUMN `use_balance`  decimal(10,2) NULL COMMENT '余额分摊金额' AFTER `use_coupon_price`;
-ADD COLUMN `growth_value`  int(11) NULL COMMENT '用户成长值' AFTER `use_balance`;
